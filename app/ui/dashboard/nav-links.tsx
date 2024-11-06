83a24d17bfb11c1933952a9bec52e8c3cef93cdd
Oluwafemi Abraham Amoo <96547407+Pappy-gamer@users.noreply.github.com>
2024-11-06 03:11:30 +0100

    Update2 nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -38,8 +38,11 @@ export default function NavLinks() {
                   'bg-sky-100 text-blue-600': pathname === link.href,
                 },
             )} >
+            <a>
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
+            </a>
+            
           </Link>
         );
       })}
